7cc7ef31a41936fea313bff55a06978a870f4b9c
Aditya Kumar Shaw <112172428+aditya-githb@users.noreply.github.com>
2024-10-15 04:39:31 +0530

    Create circular_linked_list_traversal.c

---
+++ circular_linked_list/circular_linked_list_traversal.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    struct node
+    {
+        int data;
+        struct node *next;
+    };
+
+    struct node *head = NULL, *newnode, *temp, *tail;
+    int choice = 1;
+
+    while (choice)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+
+        printf("enter data: ");
+        scanf("%d", &newnode->data);
+
+        newnode->next = NULL;
+
+        if (head == NULL)
+        {
+            head = tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = newnode;
+        }
+
+        tail->next = head;
+
+        printf("Do you want to continue(0,1): ");
+        scanf("%d", &choice);
+    }
+
+    printf("Circular Linked List: ");
+    temp = head;
+
+    while (temp->next != head)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("%d ", temp->data);
+
+    return 0;
+}
